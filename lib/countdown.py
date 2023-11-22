8c7a90d522311ac765b6132e13cb4d4dd870bf4c
Conrad Etherington <conrad.etherington@gmail.com>
2023-11-22 15:49:51 -0700

    all test passing

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,16 @@
 # your code goes here!
+import time
+def countdown(number):
+    pass
+    while number > 0:
+        print(f"{number} SECOND(S)!")
+        number -= 1
+    print("HAPPY NEW YEAR!")
+
+def countdown_with_sleep(number):
+    pass
+    while number > 0:
+        print(f"{number} SECOND(S)!")
+        number -= 1
+        time.sleep(1)
+    print("HAPPY NEW YEAR!")
